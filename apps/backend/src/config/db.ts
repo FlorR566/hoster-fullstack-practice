6ef9fb5f630fc623209d4dc7a9c48a5301176036
Estanislao <estanislaotorres@gmail.com>
2026-02-13 18:39:45 -0300

    modificacion y funciona

--- apps/backend/src/config/db.ts
+++ apps/backend/src/config/db.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv"
 dotenv.config()
 
-export const db = new Sequelize( process.env.DATABASE_URL , {
+export const db = new Sequelize( process.env.URL_DATABASE , {
     models: [__dirname + '/../models/**/*'],
     logging: false,
     dialectOptions:{
