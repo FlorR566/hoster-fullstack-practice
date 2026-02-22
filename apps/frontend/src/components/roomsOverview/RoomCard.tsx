cbaa0e7e37fb682c256d95175b06e00bb74a8836
FlorR566 <florodriguez566@gmail.com>
2026-02-22 00:04:55 -0300

    feat: implement room details modal

--- apps/frontend/src/components/roomsOverview/RoomCard.tsx
+++ apps/frontend/src/components/roomsOverview/RoomCard.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { User } from "lucide-react";
+import Modal from "./Modal";
+import {
+	Bath,
+	BedDouble,
+	ShowerHead,
+	SoapDispenserDroplet,
+} from "lucide-react";
 
 const STATUS_STYLES = {
-	ocupada:
+	Ocupada:
 		"border-[var(--light-status-ocupied)] bg-[color-mix(in_srgb,var(--light-status-ocupied),transparent_80%)]",
-	limpieza:
+	Limpieza:
 		"border-[var(--light-status-pending)] bg-[color-mix(in_srgb,var(--light-status-pending),transparent_80%)] ",
-	disponible:
+	Disponible:
 		"border-[var(--light-status-completed)] bg-[color-mix(in_srgb,var(--light-status-completed),transparent_80%)] ",
 };
 
 const TYPE_BORDER = {
-	estandar: "border-l-[var(--light-chart1)]",
-	deluxe: "border-l-[var(--light-chart2)]",
-	presidencial: "border-l-[var(--light-chart3)]",
+	Estandar: "border-l-[var(--light-chart1)]",
+	Deluxe: "border-l-[var(--light-chart2)]",
+	Presidencial: "border-l-[var(--light-chart3)]",
 };
 
 interface RoomCardProps {
@@ -24,12 +31,49 @@ interface RoomCardProps {
 }
 
 const RoomCard: React.FC<RoomCardProps> = ({ id, type, status, capacity }) => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
 	return (
 		<div className="group">
-			{" "}
+			{/*Modal */}
+			<Modal
+				isOpen={isModalOpen}
+				onClose={() => setIsModalOpen(false)}
+				title={id}
+			>
+				<div className="flex flex-col justify-between items-start gap-2 font-['Poppins'] text-[20px]">
+					<span
+						className={` px-2 py-0.5 rounded-full uppercase border ${STATUS_STYLES[status]}`}
+					>
+						{status === "Limpieza" ? "En limpieza" : status}
+					</span>
+					{type}
+					<div className="flex gap-2">
+						{Array.from({ length: capacity }).map((_, i) => (
+							<User key={i} size={23} className="text-[var(--light-text)]" />
+						))}
+					</div>
+					<div className="flex gap-2">
+						<ShowerHead size={22} />
+						<BedDouble size={22} />
+						<SoapDispenserDroplet size={22} />
+						<Bath size={22} />
+					</div>
+
+					<p>Precio por noche: $ 75.000</p>
+					<p>
+						Descripción: Lorem ipsum dolor sit amet, consectetur adipiscing
+						elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+						aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat.
+					</p>
+				</div>
+			</Modal>
+
 			{/* Contenedor padre opcional para efectos */}
 			<div
-				className={`relative bg-[var(--light-card)] text-[var(--light-text)] rounded-2xl p-4 h-48 w-full  min-h-[250px] flex flex-col justify-between border-l-[6px] ${TYPE_BORDER[type]}`}
+				onClick={() => setIsModalOpen(true)} // AHORA ES UN CLIC
+				className={`relative cursor-pointer bg-[var(--light-card)] text-[var(--light-text)] rounded-2xl p-4 h-48 w-full  min-h-[250px] flex flex-col justify-between border-l-[6px] ${TYPE_BORDER[type]}`}
 			>
 				{/* SVG de Borde Punteado */}
 				<svg
@@ -50,7 +94,7 @@ const RoomCard: React.FC<RoomCardProps> = ({ id, type, status, capacity }) => {
 					/>
 				</svg>
 
-				<div className="relative z-10 h-full flex flex-col justify-between">
+				<div className="relative z-10 h-full flex flex-col justify-between pointer-events-none">
 					{/* Header: Iconos y Badge */}
 					<div className="flex flex-col justify-between items-start gap-2">
 						<div className="flex gap-0.5">
@@ -62,7 +106,7 @@ const RoomCard: React.FC<RoomCardProps> = ({ id, type, status, capacity }) => {
 						<span
 							className={`font-['Poppins'] text-[14px] px-2 py-0.5 rounded-full uppercase border ${STATUS_STYLES[status]}`}
 						>
-							{status === "limpieza" ? "En limpieza" : status}
+							{status === "Limpieza" ? "En limpieza" : status}
 						</span>
 					</div>
 
