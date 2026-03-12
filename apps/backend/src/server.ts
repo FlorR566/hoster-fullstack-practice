24f4e4e3c0c8039f7727529b39d488832e2d2296
Juampi095 <azarjpm15@gmail.com>
2026-03-12 11:43:19 -0300

    Se realizo el endpoint de Reporte de Mantenimiento

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,33 +1,34 @@
-import express from 'express' 
 import colors from 'colors'
+import cors from "cors"
+import express from 'express'
 import morgan from 'morgan'
 import { db } from './config/db'
 import authRouter from './routes/authRouter'
 import currencyRouter from './routes/currencyRouter'
 import guestRouter from './routes/guestRouter'
+import maintenanceReportRouter from './routes/maintenanceReportRouter'
 import methodRouter from './routes/methodRouter'
 import originRouter from './routes/originRouter'
 import paymentRouter from './routes/paymentRouter'
 import reserveRouter from './routes/reserveRouter'
 import serviceRouter from './routes/serviceRouter'
 import unitRouter from './routes/unitRouter'
-import cors from "cors";
 
 async function connectDB() {
     try {
         await db.authenticate()
         db.sync()
-        console.log( colors.blue.bold('Conexion exitosa con la BD'))
+        console.log(colors.blue.bold('Conexion exitosa con la BD'))
     } catch (error) {
         console.log(error)
-        console.log( colors.red.bold('Fallo la Conexion con la BD'))
+        console.log(colors.red.bold('Fallo la Conexion con la BD'))
     }
 }
 connectDB()
 
 const app = express()
 
-app.use(cors()); 
+app.use(cors());
 
 app.use(morgan('dev'))
 
@@ -50,5 +51,7 @@ app.use('/api/reserve', reserveRouter)
 app.use('/api/service', serviceRouter)
 
 app.use('/api/unit', unitRouter)
+
+app.use('/api/maintenance-report', maintenanceReportRouter)
 
 export default app
